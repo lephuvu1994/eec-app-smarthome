2303c3cf3bd3c188e707f0b16f5ac0f0627cf9dd
vincent.le <hamai@MacBook-Air-cua-vulewenlian94gmailcom.local>
2026-03-09 07:47:05 +0700

    Add onboarding screen

--- src/components/base/header/PrimaryHomeHeader.tsx
+++ src/components/base/header/PrimaryHomeHeader.tsx
@@ -5,47 +5,49 @@ import AntDesign from "@expo/vector-icons/AntDesign"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { useUniwind } from "uniwind"
 import { PulseDot } from "../PulseDot"
+import { useIsFirstTime } from "@/lib/hooks"
 
 export const PrimaryHeaderHome = () => {
-  const {theme} = useUniwind()
+  const { theme } = useUniwind()
   const insets = useSafeAreaInsets()
+  const [isFirstTime, setFirstTime] = useIsFirstTime()
   return (
     <View
-              className="w-full flex-row gap-2 px-4"
-              style={{
-                paddingTop: insets.top,
-              }}
-            >
-              <View className="flex-1 flex-col">
-                <TouchableOpacity className="flex-row items-center gap-2.5" onPress={() => { }}>
-                  <AntDesign name="home" size={18} color={theme === ETheme.Light ? '#1B1B1B' : '#FFFFFF'} />
-                  <Text className="text-[#1B1B1B] dark:text-white">Nhà của tôi</Text>
-                  <AntDesign className="mt-1" name="caret-down" size={16} color={theme === ETheme.Light ? '#1B1B1B' : '#FFFFFF'} />
-                </TouchableOpacity>
-                <View className="flex-row items-center gap-1">
-                  <SnownyIcon />
-                  <Text className="text-sm text-[#06B6D4] dark:text-[#06B6D4]">20°C</Text>
-                </View>
-              </View>
-              <View className="flex-1 flex-row justify-end gap-2">
-                <View className="relative h-8 w-8 items-center justify-center rounded-full bg-white/40 shadow-sm dark:bg-black/40">
-                  <BellIcon color={theme === ETheme.Light ? '#737373' : '#FFFFFF'} />
-                  <PulseDot
-                    color="#22C55E"
-                    size={8}
-                    duration={1200}
-                    style={{
-                      position: 'absolute',
-                      top: 6,
-                      right: 6,
-                    }}
-                  />
-                </View>
-    
-                <View className="h-8 w-8 items-center justify-center rounded-full bg-white/40 shadow-sm dark:bg-black/40">
-                  <AntDesign name="plus" size={16} color={theme === ETheme.Light ? '#737373' : '#FFFFFF'} />
-                </View>
-              </View>
-            </View>
+      className="w-full flex-row gap-2 px-4"
+      style={{
+        paddingTop: insets.top,
+      }}
+    >
+      <View className="flex-1 flex-col">
+        <TouchableOpacity className="flex-row items-center gap-2.5" onPress={() => { }}>
+          <AntDesign name="home" size={18} color={theme === ETheme.Light ? '#1B1B1B' : '#FFFFFF'} />
+          <Text className="text-[#1B1B1B] dark:text-white">Nhà của tôi</Text>
+          <AntDesign className="mt-1" name="caret-down" size={16} color={theme === ETheme.Light ? '#1B1B1B' : '#FFFFFF'} />
+        </TouchableOpacity>
+        <View className="flex-row items-center gap-1">
+          <SnownyIcon />
+          <Text className="text-sm text-[#06B6D4] dark:text-[#06B6D4]">20°C</Text>
+        </View>
+      </View>
+      <View className="flex-1 flex-row justify-end gap-2">
+        <View className="relative h-8 w-8 items-center justify-center rounded-full bg-white/40 shadow-sm dark:bg-black/40">
+          <BellIcon color={theme === ETheme.Light ? '#737373' : '#FFFFFF'} />
+          <PulseDot
+            color="#22C55E"
+            size={8}
+            duration={1200}
+            style={{
+              position: 'absolute',
+              top: 6,
+              right: 6,
+            }}
+          />
+        </View>
+
+        <TouchableOpacity onPress={() => setFirstTime(true)} className="h-8 w-8 items-center justify-center rounded-full bg-white/40 shadow-sm dark:bg-black/40">
+          <AntDesign name="plus" size={16} color={theme === ETheme.Light ? '#737373' : '#FFFFFF'} />
+        </TouchableOpacity>
+      </View>
+    </View>
   )
 }
