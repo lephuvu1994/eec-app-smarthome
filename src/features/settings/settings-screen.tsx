f191370d874b998f67cd9ce3e4084a9f053ec1b1
vulewenlian94@gmail.com <hamai@MacBook-Air-cua-vulewenlian94gmailcom.local>
2026-03-10 00:01:12 +0700

    clean setting

--- src/features/settings/settings-screen.tsx
+++ src/features/settings/settings-screen.tsx
@@ -3,19 +3,10 @@ import { Image } from 'expo-image';
 import { useUniwind } from 'uniwind';
 import { BaseLayout } from '@/components/layout/BaseLayout';
 import { ScrollView, View } from '@/components/ui';
-import { useUserManager } from '@/features/auth/user-store';
-import { LanguageItem } from '@/features/settings/components/language-item';
-import { ThemeItem } from '@/features/settings/components/theme-item';
 import { ETheme } from '@/types/base';
-import { AccountItem } from './components/account-item';
-import { SettingsContainer } from './components/settings-container';
-import { SettingsItem } from './components/settings-item';
 
 export function SettingsScreen() {
-  const userState = useUserManager();
   const { theme } = useUniwind();
-
-  const fullName = userState.userName;
 
   return (
     <BaseLayout>
@@ -33,28 +24,7 @@ export function SettingsScreen() {
           }}
           contentFit="contain"
         />
-        <ScrollView style={{ height: '100%', width: '100%' }}>
-          <View className="w-full flex-1 gap-4 px-4">
-            <SettingsContainer title="settings.account">
-              <AccountItem fullName={fullName} email={userState.email || userState.phone || ''} isAccountDetail={false} />
-            </SettingsContainer>
-
-            <SettingsContainer title="settings.generale">
-              <SettingsItem
-                text="settings.systemTab"
-                onPress={() => { }}
-                value={Env.EXPO_PUBLIC_VERSION}
-              />
-              <LanguageItem />
-              <ThemeItem />
-            </SettingsContainer>
-
-            <SettingsContainer title="settings.about">
-              <SettingsItem text="settings.app_name" value={Env.EXPO_PUBLIC_NAME} />
-              <SettingsItem text="settings.version" value={`${Env.EXPO_PUBLIC_VERSION} (${Env.EXPO_PUBLIC_APP_ENV})`} />
-            </SettingsContainer>
-          </View>
-        </ScrollView>
+        <View></View>
       </View>
     </BaseLayout>
   );
